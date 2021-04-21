5cd6831ef2aa9669b41f7b1c8e24ac5badb859bc
casesensative <victor.waters1@gmail.com>
2021-04-21 10:24:45 -0400

    Working on Lab

---
+++ src/components/TopicBrowser/TopicBrowser.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import EvenAndOdd from './components/Topics/EvenAndOdd';
+import FilterObject from './components/Topics/FilterObject';
+import FilterString from './components/Topics/FilterString';
+import Palindrome from './components/Topics/Palindrome';
+import Sum from './components/Topics/Sum';
+
+
+class TopicBrowser extends Component {
+  render() {
+    return (
+      <div>
+        <EvenAndOdd />
+        <FilterObject />
+        <FilterString />
+        <Palindrome />
+        <Sum />
+      </div>
+    )
+
+  }
+}
+
+export default TopicBrowser;
